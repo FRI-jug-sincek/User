ce3213ddbcdb1859e471b339aed5bbf374820533
julijanjug <julijan.jug@gmail.com>
2020-11-30 20:35:55 +0100

    Added demo/info endpoint
    1.mejnik

---
+++ api/src/main/java/si/fri/rso/samples/users/api/v1/resources/DemoResource.java
@@ -0,0 +1,55 @@
+package si.fri.rso.samples.users.api.v1.resources;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+import java.util.logging.Logger;
+
+@ApplicationScoped
+@Path("/demo")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class DemoResource {
+    private Logger log = Logger.getLogger(UsersResource.class.getName());
+
+    @Context
+    protected UriInfo uriInfo;
+
+    @GET
+    @Path("/info")
+    public Response getInfo() {
+        JSONArray imena = new JSONArray();
+        imena.put("jj6706");
+        imena.put("ms9922");
+        String opis = "Aplikacija za iskanje cimrov v študentskih stanovanjih.";
+        JSONArray mikrostoritve = new JSONArray();
+        mikrostoritve.put("http://52.188.216.238:8080/v1/users");
+        mikrostoritve.put("http://52.188.216.238:8080/v1/users");
+        JSONArray githubi = new JSONArray();
+        githubi.put("https://github.com/FRI-jug-sincek/Users");
+        githubi.put("https://github.com/FRI-jug-sincek/Apartments");
+        JSONArray dockers = new JSONArray();
+        dockers.put("https://hub.docker.com/r/frijugsincek/users");
+        dockers.put("https://hub.docker.com/r/frijugsincek/apartments");
+        String jsonString = new JSONObject()
+                .put("clani", imena)
+                .put("opis_projekta", opis)
+                .put("mikrostoritve", mikrostoritve)
+                .put("github", githubi)
+                .put("travis", new JSONArray())
+                .put("dockerhub", dockers)
+                .toString();
+        return Response.status(Response.Status.OK).entity(jsonString).build();
+    }
+}
--- models/target/classes/sql-scripts/init.sql
+++ models/target/classes/sql-scripts/init.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (name, surname, age, location, created) VALUES ('Julijan', 'Jug', 23, 3, TIMESTAMP '2006-01-01 15:36:38');
+-- INSERT INTO users (name, surname, age, location, created) VALUES ('Julijan', 'Jug', 23, 3, TIMESTAMP '2006-01-01 15:36:38');
