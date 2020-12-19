4719df15788826dff08f0d5c18f25f8f8de95b8e
julijanjug <julijan.jug@gmail.com>
2020-12-19 19:21:07 +0100

    dodan readiness check

---
+++ api/src/main/java/si/fri/rso/samples/users/api/v1/health/CustomReadyCheck.java
@@ -0,0 +1,35 @@
+package si.fri.rso.samples.users.api.v1.health;
+
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import si.fri.rso.samples.users.config.RestProperties;
+import si.fri.rso.samples.users.lib.User;
+import si.fri.rso.samples.users.services.beans.UsersBean;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@Readiness
+@ApplicationScoped
+public class CustomReadyCheck implements HealthCheck {
+
+    @Inject
+    private UsersBean usersBean;
+
+    @Inject
+    private RestProperties restProperties;
+
+    public HealthCheckResponse call() {
+
+        List<User> users = usersBean.getUsers();
+
+        if (!restProperties.getBroken()) {
+            return HealthCheckResponse.up(CustomReadyCheck.class.getSimpleName());
+        } else {
+            return HealthCheckResponse.down(CustomHealthCheck.class.getSimpleName());
+        }
+    }
+
+}
